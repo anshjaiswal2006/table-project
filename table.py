34f1a18002791087710904f0df729e364f44c90f
Ansh Raj <anshjaiswal7088@gmail.com>
2026-07-18 11:26:58 +0530

    👉 Added table.py file
    Uploaded Python script for generating multiplication tables. This file takes user input and prints the table from 1 to ........

---
+++ table.py
@@ -0,0 +1,13 @@
+print (" table 1 to infinty ")
+value = (int(input("entery your digit")))
+
+print (value , 'x 1 =' , value *1)
+print (value , 'x 2 =' , value *2)
+print (value , 'x 3 =' , value *3)
+print (value , 'x 4 =' , value *4)
+print (value , "x 5 =" , value *5)
+print (value , 'x 6 =' , value *6)
+print (value , 'x 7 =' , value *7)
+print (value , 'x 8 =' , value *8)
+print (value , 'x 9 =' , value *9)
+print (value ,'x 10 =' , value *10)
